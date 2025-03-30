c870d4edfcb5f495b46c87fdc0f3def2ea045963
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-03-30 13:42:07 +0400

    feat: new design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,19 @@ import LanguageDropdown from '../ui/LanguageDropdown';
 
 function Header() {
   return (
-    <header className=' w-full z-50 bg-gray-50/95 '>
-      <div className='max-w-[1440px] mx-auto w-full flex items-center justify-between px-6 py-4'>
+    <header className='fixed  z-50  bottom-10 w-full flex items-center justify-center '>
+      {/* <div className='max-w-[1440px] mx-auto w-full flex items-center justify-between px-6 py-4'>
         <h1 className='text-xl font-light text-gray-800 tracking-wide cursor-default'>
           Company Name
         </h1>
         <div className='flex items-center justify-center gap-20'>
+        </div>
+      </div> */}
+      <div className='flex items-center  justify-between max-w-[500px] w-full  py-3 px-5 rounded-[50px] mx-auto border-[2px] border-black bg-white text-white'>
+        <div className='ml-20'>
           <ButtonNav />
+        </div>
+        <div className=''>
           <LanguageDropdown />
         </div>
       </div>
--- src/components/magniteButtonsNav/ButtonsNav.tsx
+++ src/components/magniteButtonsNav/ButtonsNav.tsx
@@ -11,75 +11,71 @@ export default function ButtonNav() {
 
   return (
     <nav className='flex gap-6'>
-      <Framer>
-        <Link
-          href={'/'}
-          className={`text-sm font-light tracking-wide relative transition-colors duration-200 ${
-            active === 'home'
-              ? 'text-gray-900 dark:text-white'
-              : 'text-gray-600 dark:text-gray-400'
-          } hover:text-gray-900 dark:hover:text-white`}
-          onMouseEnter={() => setActive('home')}
-          onMouseLeave={() => setActive('')}
-        >
-          {t('Home')}
-          {active === 'home' && (
-            <span
-              className={`absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 rounded-full ${
-                active === 'home'
-                  ? 'bg-gray-900 dark:bg-white'
-                  : 'bg-gray-600 dark:bg-gray-400'
-              }`}
-            />
-          )}
-        </Link>
-      </Framer>
-      <Framer>
-        <Link
-          href='/about'
-          className={`text-sm font-light tracking-wide relative transition-colors duration-200 ${
-            active === 'about'
-              ? 'text-gray-900 dark:text-white'
-              : 'text-gray-600 dark:text-gray-400'
-          } hover:text-gray-900 dark:hover:text-white`}
-          onMouseEnter={() => setActive('about')}
-          onMouseLeave={() => setActive('')}
-        >
-          {t('About')}
-          {active === 'about' && (
-            <span
-              className={`absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 rounded-full ${
-                active === 'about'
-                  ? 'bg-gray-900 dark:bg-white'
-                  : 'bg-gray-600 dark:bg-gray-400'
-              }`}
-            />
-          )}
-        </Link>
-      </Framer>
-      <Framer>
-        <Link
-          href='/services'
-          className={`text-sm font-light tracking-wide relative transition-colors duration-200 ${
-            active === 'services'
-              ? 'text-gray-900 dark:text-white'
-              : 'text-gray-600 dark:text-gray-400'
-          } hover:text-gray-900 dark:hover:text-white`}
-          onMouseEnter={() => setActive('services')}
-          onMouseLeave={() => setActive('')}
-        >
-          {t('Services')}
-          {active === 'services' && (
-            <span
-              className={`absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 rounded-full ${
-                active === 'services'
-                  ? 'bg-gray-900 dark:bg-white'
-                  : 'bg-gray-600 dark:bg-gray-400'
-              }`}
-            />
-          )}
-        </Link>
-      </Framer>
+      <Link
+        href={'/'}
+        className={`text-sm font-light tracking-wide relative transition-colors duration-200 ${
+          active.includes('home')
+            ? 'text-black dark:text-white'
+            : 'text-gray-600 dark:text-gray-400'
+        } hover:text-gray-900 dark:hover:text-white`}
+        onMouseEnter={() => setActive('home')}
+        onMouseLeave={() => setActive('')}
+      >
+        {t('Home')}
+        {active === 'home' && (
+          <span
+            className={`absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 rounded-full ${
+              active === 'home'
+                ? 'bg-gray-900 dark:bg-white'
+                : 'bg-gray-600 dark:bg-gray-400'
+            }`}
+          />
+        )}
+      </Link>
+
+      <Link
+        href='/about'
+        className={`text-sm font-light tracking-wide relative transition-colors duration-200 ${
+          active.includes('about')
+            ? 'text-black dark:text-white'
+            : 'text-gray-600 dark:text-gray-400'
+        } hover:text-gray-900 dark:hover:text-white`}
+        onMouseEnter={() => setActive('about')}
+        onMouseLeave={() => setActive('')}
+      >
+        {t('About')}
+        {active === 'about' && (
+          <span
+            className={`absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 rounded-full ${
+              active === 'about'
+                ? 'bg-gray-900 dark:bg-white'
+                : 'bg-gray-600 dark:bg-gray-400'
+            }`}
+          />
+        )}
+      </Link>
+
+      <Link
+        href='/services'
+        className={`text-sm font-light tracking-wide relative transition-colors duration-200 ${
+          active === 'services'
+            ? 'text-gray-100 dark:text-white'
+            : 'text-black dark:text-gray-400 '
+        } hover:text-gray-900 dark:hover:text-white`}
+        onMouseEnter={() => setActive('services')}
+        onMouseLeave={() => setActive('')}
+      >
+        {t('Services')}
+        {active === 'services' && (
+          <span
+            className={`absolute -bottom-1 left-1/2 transform -translate-x-1/2 w-1 h-1 rounded-full ${
+              active === 'services'
+                ? 'bg-gray-900 dark:bg-white'
+                : 'bg-gray-600 dark:bg-gray-400 '
+            }`}
+          />
+        )}
+      </Link>
     </nav>
   );
 }
